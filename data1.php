956891fdbe439be615af71fcfdbb2d8cfa265bbc
MuchHigher <faizlidinillah23@gmail.com>
2025-10-26 10:26:58 +0700

    array dibedakan

---
+++ data1.php
@@ -0,0 +1,8 @@
+<?php
+$users = [
+                ["name" => "Jane Smith", "email" => "jane@example.com"],
+                ["name" => "Michael Johnson", "email" => "michael@example.com"],
+                ["name" => "Emily Davis", "email" => "emily@example.com"],
+                ["name" => "David Wilson", "email" => "david@example.com"]
+              ];
+?>
